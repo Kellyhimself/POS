3fc48d72cec57c3f392c511ad24fd6e180d5b0a0
Kitui Kelly <kituikelly@gmail.com>
2025-06-20 03:09:12 +0300

    Unified services to support dual mode

--- src/components/admin/VatSettings.tsx
+++ src/components/admin/VatSettings.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useState, useEffect } from 'react';
 import { useUnifiedService } from '@/components/providers/UnifiedServiceProvider';
 import { Switch } from '@headlessui/react';
